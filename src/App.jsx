83baab6027c6b79fd56a32773c53c57479ac4d9a
Luke Harding <lmharding04@gmail.com>
2023-05-09 14:43:33 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import "./App.css";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [location, setLocation] = useState({});
+
+  function handleChange(event) {
+    setSearchQuery(event.target.value);
+  }
+
+  async function getLocation() {
+    try {
+      const API = `https://eu1.locationiq.com/v1/search?key=${process.env.REACT_API_KEY}&q=${searchQuery}&format=json`;
+      const res = await axios.get(API);
+      setLocation(res.data[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className="App">
       <h1>City Explorer</h1>
-      <form>
-        <input />
-        <button placeholder="Place name">Explore</button>
-      </form>
+      <input onChange={handleChange} placeholder="Place name" />
+      <button onClick={getLocation}>Explore</button>
+      <h2>{location.display_name}</h2>
     </div>
   );
 }
