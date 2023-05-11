06051e05d2fa9e6c52ceeeb58ab2d2b500972d37
Luke Harding <lmharding04@gmail.com>
2023-05-11 15:41:47 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ function App() {
   const [searchQuery, setSearchQuery] = useState("");
   const [location, setLocation] = useState({});
   const [map, setMap] = useState("");
+  const [apiError, setApiError] = useState("");
+  const [weather, setWeather] = useState([]);
 
   function handleChange(event) {
     setSearchQuery(event.target.value);
@@ -15,23 +17,47 @@ function App() {
     try {
       const API = `https://eu1.locationiq.com/v1/search?key=${process.env.REACT_APP_API_KEY}&q=${searchQuery}&format=json`;
       const res = await axios.get(API);
-      setLocation(res.data[0]);
-      getMap(res.data[0]);
+      const newLocation = res.data[0];
+      setLocation(newLocation);
+      getMap(newLocation);
+      getWeather(newLocation);
+      setApiError("");
     } catch (error) {
       console.log(error);
+      setApiError(error.message);
+      setLocation({});
+      setWeather([]);
+      setMap("");
     }
   }
 
-  async function getMap(data) {
-    const API = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_API_KEY}&center=${data.lat},${data.lon}&zoom=11`;
+  async function getMap(newLocation) {
+    const API = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_API_KEY}&center=${newLocation.lat},${newLocation.lon}&zoom=15`;
     setMap(API);
+  }
+
+  async function getWeather(newLocation) {
+    try {
+      const API = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}`;
+      const res = await axios.get(API);
+      console.log(res.data);
+      setWeather(res.data);
+    } catch (error) {
+      console.log(error);
+      setWeather([]);
+    }
   }
 
   return (
     <div className="App">
       <h1>City Explorer</h1>
-      <input onChange={handleChange} placeholder="Place name" />
-      <button onClick={getLocation}>Explore</button>
+      <div className="search">
+        <input onChange={handleChange} placeholder="Place name" />
+        <button className="btn" onClick={getLocation}>
+          Explore
+        </button>
+      </div>
+      <p>{apiError}</p>
       <h2>{location.display_name}</h2>
       <div className="data">
         <p>Latitude = {location.lat}</p>
